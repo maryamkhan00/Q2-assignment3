b48593f58c967201ebfd5667cee29ad22171bb2b
Maryam Khan <crystal1468592@gmail.com>
2024-10-03 14:11:40 +0500

    fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,10 @@ import Footer from "@/components/footer"
 export default function About(){
     return (
         <div>
-            <Header/>
-            <h1 className="About">Hi! I'm Maryam from Sunday morning class.</h1>
+            <Header/>  
+            <div>
+            <h1 className="About"> Hi! I'm Maryam from Sunday morning class.</h1>
+            </div>          
             <Footer/>
         </div>
     )
--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -5,7 +5,9 @@ export default function Contact(){
     return (
         <div>
             <Header/>
+            <div>
             <h1 className="contactUs">You can contact me through my student ID 00142635 </h1>
+            </div>
             <Footer/>
         </div>
     )
