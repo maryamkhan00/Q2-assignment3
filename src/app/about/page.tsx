0634f0d4a86e815ae9a0c618c1e8dfd007e436d8
Maryam Khan <crystal1468592@gmail.com>
2024-11-07 13:23:20 +0500

    change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function about(){
     return (
             <div>
-            <h1 className="about"> Hi! I&apos;m Maryam from Sunday morning class.</h1>
+            <h1 className="about"> Hi! I&apos;m Maryam from Sunday morning class</h1>
             </div>       
     )
 }
