ebea158d91beb5b5fdc651ec4fae712456591c63
Maryam Khan <crystal1468592@gmail.com>
2024-11-07 13:08:34 +0500

    improvement 3

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function about(){
     return (
             <div>
-            <h1 className="about"> Hi! I'm Maryam from Sunday morning class.</h1>
+            <h1 className="about"> Hi! Im Maryam from Sunday morning class.</h1>
             </div>       
     )
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,13 +2,13 @@ export default function portfolio(){
     return (
             <div className="portfolio">
                 <ul>
-                    <li><span className="point">Skills:</span> I'm starting out as a front end developer and 
-                    I excel in TypeScript, HTML and CSS</li>
+                    <li><span className="point">Skills:</span> Im starting out as a front end developer and 
+                    I excel in TypeScript HTML and CSS</li>
                     <br/>
                     <li><span className="point">Experience:</span> I have worked on multiple different cli projects 
-                    using TypeScript and NodeJS and I'm currently working on nextJS</li>
+                    using TypeScript and NodeJS and I am currently working on nextJS</li>
                     <br/>
-                    <li><span className="point">About Project:</span> This is my Portfolio website, made by using NextJS
+                    <li><span className="point">About Project:</span> This is my Portfolio website made by using NextJS
                     </li>
                 </ul>
             </div>
