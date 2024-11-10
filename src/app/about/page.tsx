52e6f59d693036f07fe3b3ee8051e1d92b3382c3
Maryam Khan <crystal1468592@gmail.com>
2024-11-10 19:46:08 +0500

    mobile responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
 export default function about(){
     return (
+        <div>
             <div className="about-container">
             <h1 className="about"> Hi! I&apos;m Maryam from Sunday morning class</h1>
-            </div>       
+            </div>
+        </div>       
     )
 }
--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,9 @@
 export default function contact(){
     return (
+        <div>
             <div className="contact-container">
             <h1 className="contactus">You can contact me through my student ID 00142635 </h1>
             </div>
+        </div>   
     )
 }
