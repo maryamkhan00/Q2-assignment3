7f7f734495e00990ca9a91bf6345ba938e87bf2b
Maryam Khan <crystal1468592@gmail.com>
2024-11-10 19:44:08 +0500

    mobile interface

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function about(){
     return (
-            <div>
+            <div className="about-container">
             <h1 className="about"> Hi! I&apos;m Maryam from Sunday morning class</h1>
             </div>       
     )
